838bc0c0685fe677be882f6c419d388fbe76c7f9
Roberto Cabal <robertocabal5@gmail.com>
2022-09-20 08:41:44 -0500

    simple convolutions

--- intro/conv1d.py
+++ intro/conv1d.py
@@ -6,8 +6,7 @@ A = torch.tensor([[[1,0,2,0,3,0]]]).float()
 conv1d = torch.nn.Conv1d(1,out_channels=2,kernel_size=3,bias=False)
 
 conv1d.weight = Parameter(torch.tensor([
-    [[1,0,-1]],
-    [[0,2,0]],
+    [[1,0,-1]], [[0,2,0]],
 ]).float())
 
 output = conv1d(A)
