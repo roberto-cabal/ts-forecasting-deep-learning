5b6f151dc29ab9d45ba0a745fd01fac7871f0f31
Roberto Cabal <cabal.lopez.roberto@gmail.com>
2022-09-21 08:28:07 -0500

    max pooling

--- intro/max_pool_2d.py
+++ intro/max_pool_2d.py
@@ -1,11 +1,11 @@
 import torch
 
-A = torch.tensor([[
+A = torch.tensor([[[
     [1,2,-1,1],
     [0,1,-2,-2],
     [3,0,5,0],
     [0,1,4,-3],
-]]).float()
+]]]).float()
 
 max_pool = torch.nn.MaxPool2d(2)
 
