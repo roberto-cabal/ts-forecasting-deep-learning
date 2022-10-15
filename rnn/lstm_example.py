fcef7aad505e8899e735f5131d5630c61652f048
Roberto Cabal <cabal.lopez.roberto@gmail.com>
2022-10-15 11:18:03 -0500

    add competing models to lsmt

--- rnn/lstm_example.py
+++ rnn/lstm_example.py
@@ -8,6 +8,7 @@ import torch
 from sklearn.preprocessing import MinMaxScaler
 from lstm import LSTM
 from get_data import *
+from competing_models import *
 
 # seed
 random.seed(1)
@@ -35,6 +36,24 @@ x_train, x_val, x_test, y_train, y_val, y_test = get_training_datasets(scaled_ts
 # --- define model
 model = LSTM(hidden_size = lstm_hidden_size)
 model.train()
+
+# --- ALTERNATIVE MODELS
+dummy_predictor = DummyPredictor()
+sarima_predictor = SarimaxPredictor()
+hwes_predictor = HwesPredictor()
+
+yhat_dummy = dummy_predictor(x_test)
+yhat_sarima = sarima_predictor(x_test)
+yhat_hwes = hwes_predictor(x_test)
+
+yhat_dummy = scaler.inverse_transform(yhat_dummy.reshape(-1,1)).reshape(-1)
+yhat_sarima = scaler.inverse_transform(yhat_sarima.reshape(-1,1)).reshape(-1)
+yhat_hwes = scaler.inverse_transform(yhat_hwes.reshape(-1,1)).reshape(-1)
+
+real = scaler.inverse_transform(y_test.tolist()).reshape(-1)
+ms_error_dummy = np.mean((real-yhat_dummy)**2)
+ms_error_sarima = np.mean((real-yhat_sarima)**2)
+ms_error_hwes = np.mean((real-yhat_hwes)**2)
 
 # --- training
 optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
@@ -70,7 +89,7 @@ for t in range(training_epochs):
               f'val: - {round(val_loss.item(), 4)}')
 
 # --- predict on test set
-use_warm_up = True
+use_warm_up = False
 
 if use_warm_up:
     best_model.eval()
@@ -104,11 +123,18 @@ plt.xlabel('Epoch')
 plt.legend()
 plt.show()
 
+msse = ms_error/ms_error_dummy
 plt.figure()
-plt.title(f"Test dataset: MSE = {ms_error}")
-plt.plot(real, label = 'real')
-plt.plot(predicted, label = 'predicted')
+plt.title(f"Test dataset: MSE = {ms_error}, MSSE = {msse}")
+plt.plot(real,label='real')
+plt.plot(predicted,label='predicted')
+plt.plot(yhat_dummy,label='dummy')
+plt.plot(yhat_sarima,label='sarima')
+plt.plot(yhat_hwes,label='hwes')
 plt.legend()
 plt.show()
 
-print(f'MSE = {ms_error}')
+print(f'LSTM MSE = {ms_error}')
+print(f'LSTM Dummy = {ms_error_dummy}')
+print(f'LSTM Sarima = {ms_error_sarima}')
+print(f'LSTM Hwes = {ms_error_hwes}')
